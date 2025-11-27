90441319e6ae2906d182dd64135462089a7bf723
Agust-ux <Krvaa015@osloskolen.no>
2025-11-27 09:23:49 +0100

    Ramme på tabellen

--- utstyr.sql
+++ utstyr.sql
@@ -1,1 +1,15 @@
-CREATE TABLE Utstyr_oversikt {}
+CREATE TABLE Utstyr_oversikt {
+    id INTEGER PRIMARY KEY, 
+    navn TEXT, 
+    antall INTEGER, 
+    status TEXT,
+    utstyr TEXT
+    }
+
+    INSERT INTO Utstyr_oversikt VALUES {
+        1,
+        LENOVO,
+        3,
+        Fungerende,
+        Tastatur
+    }
